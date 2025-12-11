bd276764ec0341850ba549e53b24836496c4ddfa
Bartosz Koza <kozabartosz01@gmail.com>
2025-12-11 14:18:09 +0100

    Added GlobalStyle.js

---
+++ src/GlobalStyle.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+
+const GlobalStyle = createGlobalStyle`
+    html {
+    box-sizing: border-box;
+  }
+
+  *,
+  ::after,
+  ::before {
+    box-sizing: inherit;
+  }
+
+  body {
+  
+    margin: auto;
+    background-color: #eee;
+    font-family: sans-serif;
+  }
+`
+
+export default GlobalStyle;

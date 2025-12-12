49f960339aa1769d6af2212aea00927414961953
Bartosz Koza <kozabartosz01@gmail.com>
2025-12-12 16:01:05 +0100

    added styled to Navigation NavigationItem NavigationWrapper and Input

---
+++ src/components/Navigation/styled.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const NavigationItem = styled.li`
+  cursor: pointer;
+  padding: 8px 24px 8px 24px;
+  border: 1px;
+
+  border-radius: 24px;
+  border-color: white;
+`;
+
+export const Navigation = styled.ul`
+  background-color: #18181b;
+  max-width: 100%;
+  height: 94px;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+`;
+
+export const NavigationWrapper = styled.ul`
+  list-style: none;
+  color: white;
+  size: 21px;
+  text-align: start;
+  max-width: 1368px;
+  width: 100%;
+  margin: auto;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+export const Input = styled.input`
+  max-width: 100%;
+  width: 432px;
+  height: 48px;
+  border-radius: 33px;
+  border: 1px;
+  margin-left: 411px;
+`;
+
+export const NavigationTitle = styled.h1``;

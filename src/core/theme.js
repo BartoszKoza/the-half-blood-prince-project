4b6919f75810fe7d80d413a5099a9c5a0a4c1a78
Sandra Masternak <sandra.masternak1@gmail.com>
2025-12-15 03:33:53 +0100

    moved theme.js to folder core

---
+++ src/core/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+  color: {
+    white: "#FFFFFF",
+    whisper: "#F5F5FA",
+    mystic: "#E4E6F0",
+    pattensblue: "#D6E4FF",
+    snuff: "#DDDDEE",
+    waterloo: "#7E839A",
+    stormgrey: "#74788B",
+    mineshaft: "#333333",
+    valentino: "#230B34",
+    haiti: "#200E32",
+    woodsmoke: "#18181B",
+    black: "#000000",
+    blackpearl: "#03121F",
+    candlelight: "#FCD420",
+    sceniceblue: "#0044CC",
+  },
+};

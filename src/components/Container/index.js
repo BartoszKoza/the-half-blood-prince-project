5fa5d06bcd3bc075fb895083455d60db02de4a3b
Sandra Masternak <sandra.masternak1@gmail.com>
2025-12-15 03:34:41 +0100

    added max-width, padding, @media & margin: 0

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  
-    margin: auto;
+  max-width: 1320px;
+  margin: 0 auto;
+  padding: 24px;
+
+  @media (max-width: 767px) {
+    padding: 16px;
+  }
 `;

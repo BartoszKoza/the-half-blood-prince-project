75473be7bcb88fd418d47e1f48a56a97d61e1aff
Sandra Masternak <sandra.masternak1@gmail.com>
2025-12-15 03:47:15 +0100

    created inedx & styled for MovieList

---
+++ src/features/MovieList/styled.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const Page = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+export const SectionTitle = styled.h2`
+  font-size: 36px;
+  font-weight: 600;
+  line-height: 1.2;
+  padding-bottom: 24px;
+  text-align: left;
+`;
+
+export const MoviesGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 24px;
+
+  @media (max-width: 1279px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: 1023px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 767px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+
+export const Star = styled.img`
+  width: 24px;
+  height: 24px;
+  flex-shrink: 0;
+`;

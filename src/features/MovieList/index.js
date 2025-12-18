5e85e82cd2164cb9144b66d3a80ddb8c35088708
Bartosz Koza <kozabartosz01@gmail.com>
2025-12-18 14:41:56 +0100

    add URL-based search and pagination to MovieList

--- src/features/MovieList/index.js
+++ src/features/MovieList/index.js
@@ -1,28 +1,45 @@
 import { useEffect, useState } from "react";
 import Tile from "../../components/Tile";
-import { getPopularMovies, getGenres } from "../../core/api";
+import { getPopularMovies, getGenres, searchMovies } from "../../core/api";
 import { Page, MoviesGrid, SectionTitle } from "./styled";
 import { Pagination } from "../../components/Pagination";
+import { useHistory, useLocation } from "react-router-dom";
 
 const MOVIES_PER_PAGE = 8;
 const API_PAGE_SIZE = 20;
 
 export default function MovieList() {
+
+  const location = useLocation();
+  const history = useHistory();
+  
+  const params = new URLSearchParams(location.search);
+  const searchQuery = params.get("search");
+  const urlPage = Number(params.get("page")) || 1;
+  
   const [movies, setMovies] = useState([]);
   const [genreMap, setGenreMap] = useState({});
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(urlPage);
   const [totalPages, setTotalPages] = useState(1);
+
+  
+
+  useEffect(() => {
+    setPage(urlPage);
+  }, [urlPage]);
 
   useEffect(() => {
     loadData(page);
-  }, [page]);
+  }, [page, searchQuery]);
 
   const loadData = async (uiPage) => {
     const apiPage = Math.ceil(
       (uiPage * MOVIES_PER_PAGE) / API_PAGE_SIZE
     );
 
-    const moviesData = await getPopularMovies(apiPage);
+    const moviesData = searchQuery
+    ? await searchMovies(searchQuery, apiPage)
+    : await getPopularMovies(apiPage);
     const genresData = await getGenres();
 
     if (moviesData) {
@@ -65,7 +82,11 @@ export default function MovieList() {
       <Pagination
         currentPage={page}
         totalPages={totalPages}
-        onPageChange={setPage}
+        onPageChange={(p) => {
+          const next = new URLSearchParams(location.search);
+          next.set("page",p);
+          history.push({search:next.toString()});
+        }}
       />
     </Page>
   );

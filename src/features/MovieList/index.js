27ff465fd66c611e531a86d769f7326af00dff15
Kamil <kam.barabasz@gmail.com>
2025-12-20 00:00:44 +0100

    Add clickable links from MovieList to MovieDetails

--- src/features/MovieList/index.js
+++ src/features/MovieList/index.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Tile from "../../components/Tile";
 import { getPopularMovies, getGenres } from "../../core/api";
 import { Page, MoviesGrid, SectionTitle } from "./styled";
 import { Pagination } from "../../components/Pagination";
 
+
 const MOVIES_PER_PAGE = 8;
 const API_PAGE_SIZE = 20;
+
 
 export default function MovieList() {
   const [movies, setMovies] = useState([]);
@@ -13,35 +16,44 @@ export default function MovieList() {
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
 
+
   useEffect(() => {
     loadData(page);
   }, [page]);
+
 
   const loadData = async (uiPage) => {
     const apiPage = Math.ceil(
       (uiPage * MOVIES_PER_PAGE) / API_PAGE_SIZE
     );
 
+
     const moviesData = await getPopularMovies(apiPage);
     const genresData = await getGenres();
+
 
     if (moviesData) {
       const startIndex =
         ((uiPage - 1) * MOVIES_PER_PAGE) % API_PAGE_SIZE;
+
 
       const pageMovies = moviesData.results.slice(
         startIndex,
         startIndex + MOVIES_PER_PAGE
       );
 
+
       setMovies(pageMovies);
+
 
       const uiTotalPages = Math.ceil(
         (moviesData.total_results || 0) / MOVIES_PER_PAGE
       );
 
+
       setTotalPages(Math.min(uiTotalPages, 500));
     }
+
 
     if (genresData?.genres) {
       const map = {};
@@ -52,15 +64,24 @@ export default function MovieList() {
     }
   };
 
+
   return (
     <Page>
       <SectionTitle>Popular Movies</SectionTitle>
 
+
       <MoviesGrid>
         {movies.map((movie) => (
-          <Tile key={movie.id} movie={movie} genreMap={genreMap} />
+          <Link
+            key={movie.id}
+            to={`/movies/${movie.id}`}
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <Tile movie={movie} genreMap={genreMap} />
+          </Link>
         ))}
       </MoviesGrid>
+
 
       <Pagination
         currentPage={page}

eae77d7a2db8e94cd9f6db8946f8b76a2c552251
Sandra Masternak <sandra.masternak1@gmail.com>
2025-12-20 23:16:06 +0100

    added paginated movie list with API page mapping, fetch and map movie genres

--- src/features/MovieList/index.js
+++ src/features/MovieList/index.js
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import Tile from "../../components/Tile";
 import { getPopularMovies, getGenres } from "../../core/api";
-import { Page, MoviesGrid, SectionTitle } from "./styled";
+import { Page, MoviesGrid, SectionTitle, Content } from "./styled";
 import { Pagination } from "../../components/Pagination";
 
 const MOVIES_PER_PAGE = 8;
@@ -14,20 +14,28 @@ export default function MovieList() {
   const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
-    loadData(page);
-  }, [page]);
+    const fetchGenres = async () => {
+      const genresData = await getGenres();
 
-  const loadData = async (uiPage) => {
-    const apiPage = Math.ceil(
-      (uiPage * MOVIES_PER_PAGE) / API_PAGE_SIZE
-    );
+      if (genresData?.genres) {
+        const map = {};
+        genresData.genres.forEach((g) => {
+          map[g.id] = g.name;
+        });
+        setGenreMap(map);
+      }
+    };
+
+    fetchGenres();
+  }, []);
+
+  const loadData = useCallback(async (uiPage) => {
+    const apiPage = Math.ceil((uiPage * MOVIES_PER_PAGE) / API_PAGE_SIZE);
 
     const moviesData = await getPopularMovies(apiPage);
-    const genresData = await getGenres();
 
     if (moviesData) {
-      const startIndex =
-        ((uiPage - 1) * MOVIES_PER_PAGE) % API_PAGE_SIZE;
+      const startIndex = ((uiPage - 1) * MOVIES_PER_PAGE) % API_PAGE_SIZE;
 
       const pageMovies = moviesData.results.slice(
         startIndex,
@@ -42,25 +50,23 @@ export default function MovieList() {
 
       setTotalPages(Math.min(uiTotalPages, 500));
     }
+  }, []);
 
-    if (genresData?.genres) {
-      const map = {};
-      genresData.genres.forEach((g) => {
-        map[g.id] = g.name;
-      });
-      setGenreMap(map);
-    }
-  };
+  useEffect(() => {
+    loadData(page);
+  }, [page, loadData]);
 
   return (
     <Page>
-      <SectionTitle>Popular Movies</SectionTitle>
+      <Content>
+        <SectionTitle>Popular movies</SectionTitle>
 
-      <MoviesGrid>
-        {movies.map((movie) => (
-          <Tile key={movie.id} movie={movie} genreMap={genreMap} />
-        ))}
-      </MoviesGrid>
+        <MoviesGrid>
+          {movies.map((movie) => (
+            <Tile key={movie.id} movie={movie} genreMap={genreMap} />
+          ))}
+        </MoviesGrid>
+      </Content>
 
       <Pagination
         currentPage={page}

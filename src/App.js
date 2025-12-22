56872d57c9a584035a0643e8e9fe3494525ea28b
Kamil <kam.barabasz@gmail.com>
2025-12-22 23:43:28 +0100

    Add Person List feature with styled tiles and theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NavigationBar from "./components/Navigation";
 import MovieList from "./features/MovieList";
+import PersonList from "./features/PersonList";
 
 export default function App() {
-  return (
-    <>
-      <NavigationBar />
-      <MovieList />
-    </>
-  );
+    return (
+        <BrowserRouter>
+            <NavigationBar />
+
+            <Routes>
+                <Route path="/" element={<Navigate to="/movies" replace />} />
+
+                <Route path="/movies" element={<MovieList />} />
+
+                <Route path="/people" element={<PersonList />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }

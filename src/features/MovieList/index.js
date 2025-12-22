83d874cdda28e4f488f12096679fea4aac4333a3
Kamil <kam.barabasz@gmail.com>
2025-12-22 22:20:37 +0100

    Refactor MovieList: extract StyledLink to styled.js

--- src/features/MovieList/index.js
+++ src/features/MovieList/index.js
@@ -1,93 +1,71 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import Tile from "../../components/Tile";
 import { getPopularMovies, getGenres } from "../../core/api";
-import { Page, MoviesGrid, SectionTitle } from "./styled";
+import { Page, MoviesGrid, SectionTitle, StyledLink } from "./styled";
 import { Pagination } from "../../components/Pagination";
-
 
 const MOVIES_PER_PAGE = 8;
 const API_PAGE_SIZE = 20;
 
-
 export default function MovieList() {
-  const [movies, setMovies] = useState([]);
-  const [genreMap, setGenreMap] = useState({});
-  const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
+    const [movies, setMovies] = useState([]);
+    const [genreMap, setGenreMap] = useState({});
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
 
+    useEffect(() => {
+        loadData(page);
+    }, [page]);
 
-  useEffect(() => {
-    loadData(page);
-  }, [page]);
+    const loadData = async (uiPage) => {
+        const apiPage = Math.ceil((uiPage * MOVIES_PER_PAGE) / API_PAGE_SIZE);
 
+        const moviesData = await getPopularMovies(apiPage);
+        const genresData = await getGenres();
 
-  const loadData = async (uiPage) => {
-    const apiPage = Math.ceil(
-      (uiPage * MOVIES_PER_PAGE) / API_PAGE_SIZE
+        if (moviesData) {
+            const startIndex = ((uiPage - 1) * MOVIES_PER_PAGE) % API_PAGE_SIZE;
+
+            const pageMovies = moviesData.results.slice(
+                startIndex,
+                startIndex + MOVIES_PER_PAGE
+            );
+
+            setMovies(pageMovies);
+
+            const uiTotalPages = Math.ceil(
+                (moviesData.total_results || 0) / MOVIES_PER_PAGE
+            );
+
+            setTotalPages(Math.min(uiTotalPages, 500));
+        }
+
+        if (genresData?.genres) {
+            const map = {};
+            genresData.genres.forEach((g) => {
+                map[g.id] = g.name;
+            });
+            setGenreMap(map);
+        }
+    };
+
+    return (
+        <Page>
+            <SectionTitle>Popular Movies</SectionTitle>
+
+            <MoviesGrid>
+                {movies.map((movie) => (
+                    <StyledLink key={movie.id} to={`/movies/${movie.id}`}>
+                        <Tile movie={movie} genreMap={genreMap} />
+                    </StyledLink>
+                ))}
+            </MoviesGrid>
+
+            <Pagination
+                currentPage={page}
+                totalPages={totalPages}
+                onPageChange={setPage}
+            />
+        </Page>
     );
-
-
-    const moviesData = await getPopularMovies(apiPage);
-    const genresData = await getGenres();
-
-
-    if (moviesData) {
-      const startIndex =
-        ((uiPage - 1) * MOVIES_PER_PAGE) % API_PAGE_SIZE;
-
-
-      const pageMovies = moviesData.results.slice(
-        startIndex,
-        startIndex + MOVIES_PER_PAGE
-      );
-
-
-      setMovies(pageMovies);
-
-
-      const uiTotalPages = Math.ceil(
-        (moviesData.total_results || 0) / MOVIES_PER_PAGE
-      );
-
-
-      setTotalPages(Math.min(uiTotalPages, 500));
-    }
-
-
-    if (genresData?.genres) {
-      const map = {};
-      genresData.genres.forEach((g) => {
-        map[g.id] = g.name;
-      });
-      setGenreMap(map);
-    }
-  };
-
-
-  return (
-    <Page>
-      <SectionTitle>Popular Movies</SectionTitle>
-
-
-      <MoviesGrid>
-        {movies.map((movie) => (
-          <Link
-            key={movie.id}
-            to={`/movies/${movie.id}`}
-            style={{ textDecoration: "none", color: "inherit" }}
-          >
-            <Tile movie={movie} genreMap={genreMap} />
-          </Link>
-        ))}
-      </MoviesGrid>
-
-
-      <Pagination
-        currentPage={page}
-        totalPages={totalPages}
-        onPageChange={setPage}
-      />
-    </Page>
-  );
 }
--- src/features/MovieList/styled.js
+++ src/features/MovieList/styled.js
@@ -1,40 +1,46 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Page = styled.div`
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 20px;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 20px;
 `;
 
 export const SectionTitle = styled.h2`
-  font-size: 36px;
-  font-weight: 600;
-  line-height: 1.2;
-  padding-bottom: 24px;
-  text-align: left;
+    font-size: 36px;
+    font-weight: 600;
+    line-height: 1.2;
+    padding-bottom: 24px;
+    text-align: left;
 `;
 
 export const MoviesGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 24px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 24px;
 
-  @media (max-width: 1279px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
+    @media (max-width: 1279px) {
+        grid-template-columns: repeat(3, 1fr);
+    }
 
-  @media (max-width: 1023px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
+    @media (max-width: 1023px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
 
-  @media (max-width: 767px) {
-    grid-template-columns: 1fr;
-  }
+    @media (max-width: 767px) {
+        grid-template-columns: 1fr;
+    }
 `;
 
+export const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: inherit;
+    display: block;
+`;
 
 export const Star = styled.img`
-  width: 24px;
-  height: 24px;
-  flex-shrink: 0;
+    width: 24px;
+    height: 24px;
+    flex-shrink: 0;
 `;

c9b48e0e32cdc42e595147712c5010f80d1623c4
Sandra Masternak <sandra.masternak1@gmail.com>
2025-12-27 04:06:35 +0100

    deleted repeated elements, deleted BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import NavigationBar from "./components/Navigation";
 import MovieList from "./features/MovieList";
 import PersonList from "./features/PersonList";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Navigate, Route, Routes } from "react-router-dom";
-import NavigationBar from "./components/Navigation";
-import MovieList from "./features/MovieList";
 import MovieDetails from "./features/MovieDetails";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <NavigationBar />
-
-            <Routes>
-                <Route path="/" element={<Navigate to="/movies" replace />} />
-
-                <Route path="/movies" element={<MovieList />} />
-
-                <Route path="/people" element={<PersonList />} />
-            </Routes>
-        </BrowserRouter>
-    );
-            <Routes>
-                <Route path="/" element={<MovieList />} />
-                <Route path="/movies" element={<MovieList />} />
-                <Route path="/movies/:id" element={<MovieDetails />} />
-            </Routes>
-        </BrowserRouter>
-    );
   return (
     <>
       <NavigationBar />
+
       <Routes>
+        <Route path="/" element={<Navigate to="/movies" replace />} />
+
         <Route path="/movies" element={<MovieList />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+
+        <Route path="/people" element={<PersonList />} />
+
         <Route path="*" element={<Navigate to="/movies" replace />} />
       </Routes>
     </>

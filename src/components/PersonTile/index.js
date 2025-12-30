4b4e4f1f2745329807dfcb2ed51b298ac1530fc8
Sandra Masternak <sandra.masternak1@gmail.com>
2025-12-30 02:25:51 +0100

    updated PersonTile

--- src/components/PersonTile/index.js
+++ src/components/PersonTile/index.js
@@ -1,20 +1,27 @@
-import { TileWrapper, TileImage, TileName, Placeholder } from "./styled";
+import {
+    TileWrapper,
+    TileImage,
+    Name,
+    Role,
+    NameWrapper,
+} from "./styled";
+
+import noPerson from "../../images/no-person.png";
 
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w185";
 
-export default function PersonTile({ person }) {
+export default function PersonTile({ person, role }) {
     const imageUrl = person.profile_path
         ? `${IMAGE_BASE_URL}${person.profile_path}`
-        : null;
+        : noPerson;
 
     return (
         <TileWrapper>
-            {imageUrl ? (
-                <TileImage src={imageUrl} alt={person.name} />
-            ) : (
-                <Placeholder />
-            )}
-            <TileName>{person.name}</TileName>
+            <TileImage src={imageUrl} alt={person.name} />
+            <NameWrapper>
+                <Name>{person.name}</Name>
+                {role && <Role>{role}</Role>}
+            </NameWrapper>
         </TileWrapper>
     );
 }

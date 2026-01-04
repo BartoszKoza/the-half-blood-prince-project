89300a48669b080e39a236cc95800cde50abf12a
Sandra Masternak <sandra.masternak1@gmail.com>
2026-01-05 00:46:11 +0100

    created NoResults component

---
+++ src/components/NoResults/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+  font-size: 36px;
+  font-weight: 600;
+  line-height: 1.2;
+  margin-top: 56px;
+  margin-bottom: 80px;
+  text-align: left;
+`;
+
+export const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  svg {
+    max-width: 100%;
+  }
+`;

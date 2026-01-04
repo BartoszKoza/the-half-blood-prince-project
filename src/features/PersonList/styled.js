0971a2d3b1e37fa66b97938cf4c538b0c6ea24c3
Bartosz Koza <kozabartosz01@gmail.com>
2026-01-05 00:06:47 +0100

    Align person list layout

--- src/features/PersonList/styled.js
+++ src/features/PersonList/styled.js
@@ -1,35 +1,24 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-export const Page = styled.main`
-    max-width: 1200px;
-    margin: 0 auto;
-    padding: 20px;
-`;
 
-export const SectionTitle = styled.h2`
-    font-size: 36px;
-    font-weight: 600;
-    line-height: 1.2;
-    padding-bottom: 24px;
-    text-align: left;
-`;
 
 export const PeopleGrid = styled.div`
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(6, 1fr);
     gap: 24px;
+    max-width: 1368px;
 
     @media (max-width: ${({ theme }) => theme.breakpoints.desktopSmMax}px) {
-        grid-template-columns: repeat(3, 1fr);
+        grid-template-columns: repeat(4, 1fr);
     }
 
     @media (max-width: ${({ theme }) => theme.breakpoints.tabletMax}px) {
-        grid-template-columns: repeat(2, 1fr);
+        grid-template-columns: repeat(3, 1fr);
     }
 
     @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-        grid-template-columns: 1fr;
+         grid-template-columns: repeat(2, 1fr);
     }
 `;
 
